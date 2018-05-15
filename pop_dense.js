04c9dbddf38558edb9df40f2951bfd400d19d91d
Kunal <kusdhill@ucsc.edu>
2018-05-14 21:59:43 -0700

    projection work

--- pop_dense.js
+++ pop_dense.js
@@ -1,15 +1,20 @@
 //Width and height
-var w = 500;
-var h = 500;
+var width = 500;
+var height = 500;
+
+// Define projection
+var projection = d3.geoAlbers()
+//https://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
+	.fitSize([width, height]);
 
 //Define path generator
-var path = d3.geoPath()
+var path = d3.geoPath(projection)
 
 //Create SVG element
 var svg = d3.select("body")
 			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
+			.attr("width", width)
+			.attr("height", height);
 
 //Load in GeoJSON data
 d3.json("israel.json", function(json) {
